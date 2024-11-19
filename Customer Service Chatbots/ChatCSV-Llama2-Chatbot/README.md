f7afe6acfa73d3e481ef6b292218682191c4c958
pavanraje234 <pavanrajeg234@gmail.com>
2024-11-19 11:10:17 +0530

    final project

--- Customer Service Chatbots/ChatCSV-Llama2-Chatbot/README.md
+++
@@ -1,2 +0,0 @@
-# ChatCSV-Llama2-Chatbot
-ChatCSV bot using Llama 2, Sentence Transformers, CTransformers, Langchain, and Streamlit.
--- Customer Service Chatbots/csv/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-# Send a message to the model
-response = chat_session.send_message("What is the total number of customers based on the CSV data?")
